f6cf75641ba42e8372b16651071418cb238e1316
Bruno Rosas <bruno.rosas@diputados.gob.mx>
2024-06-05 18:33:55 -0600

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	new file:   src/app/fichas/fichas.component.html 	new file:   src/app/fichas/fichas.component.spec.ts 	new file:   src/app/fichas/fichas.component.ts 	modified:   src/index.html 	modified:   src/styles.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/fichas/fichas.component.html 	modified:   src/app/fichas/fichas.component.ts

---
+++ src/app/fichas/fichas.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-fichas',
+  standalone: true,
+  imports: [],
+  templateUrl: './fichas.component.html',
+  styleUrl: './fichas.component.css'
+})
+export class FichasComponent {
+
+}
