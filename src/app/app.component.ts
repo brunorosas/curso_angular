992eac287a36602533101a23da4b4b701d34a691
Bruno Rosas <bruno.rosas@diputados.gob.mx>
2024-06-05 18:36:15 -0600

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	new file:   src/app/data/fichas-curriculares.ts 	new file:   src/app/ficha-persona/ficha-persona.component.css 	new file:   src/app/ficha-persona/ficha-persona.component.html 	new file:   src/app/ficha-persona/ficha-persona.component.spec.ts 	new file:   src/app/ficha-persona/ficha-persona.component.ts 	new file:   src/app/fichas/fichas.component.css 	modified:   src/app/fichas/fichas.component.html 	modified:   src/app/fichas/fichas.component.ts 	new file:   src/app/interfaces/ificha-persona.ts 	new file:   src/app/shared/directivas/resaltado.directive.spec.ts 	new file:   src/app/shared/directivas/resaltado.directive.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FichasComponent } from  './fichas/fichas.component';
+import { ResaltadoDirective } from  './shared/directivas/resaltado.directive'
+import { DatePipe } from  '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FichasComponent],
+  imports: [RouterOutlet, FichasComponent,ResaltadoDirective, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export  class  AppComponent {
 	title  =  'Fichas curriculares';
+  fechaHoy  =  new  Date();
 }
---
+++ src/app/data/fichas-curriculares.ts
@@ -0,0 +1,48 @@
+import { IFichaPersona } from '../interfaces/ificha-persona';
+
+export const fichasCurriculares  : IFichaPersona []= [
+    {
+        "id": 1,
+        "nombre": "Maria Morales Dominguez",
+        "ciudad": "Veracruz",
+
+        "distrito": 80,
+        "url_photo": "103_foto_chica.jpg",
+        "reelecta": true,
+        "enlicencia": false,
+        "suplente": "Francisco Bocanegra Collado"
+    },
+    {
+        "id": 2,
+        "nombre": "Sofia Burgos Linares",
+        "ciudad": "México",
+
+        "distrito": 83,
+        "url_photo": "116_foto_chica.jpg",
+        "reelecta": true,
+        "enlicencia": false,
+        "suplente": "Lorena Ruiz Morales"
+    },
+    {
+        "id": 3,
+        "nombre": "Franciso Rojas Balderas",
+        "ciudad": "Guanajuato",
+
+        "distrito": 45,
+        "url_photo": "038_foto_chica.jpg",
+        "reelecta": true,
+        "enlicencia": false,
+        "suplente": "Joaquin Doriga Martinez"
+    },
+    {
+      "id": 4,
+      "nombre": "Bruno Rosas Fernandez",
+      "ciudad": "Guanajuato",
+
+      "distrito": 45,
+      "url_photo": "040_foto_chica.jpg",
+      "reelecta": true,
+      "enlicencia": false,
+      "suplente": "Un tipo igualito a mi "
+  }
+]
